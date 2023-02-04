f3c9d0c15c76fd887087d16ccace4fad80348eb3
Vitaly Kovalev <kovalevvitalii@mail.ru>
2023-02-04 11:40:34 +0300

    Initial commit

---
+++ Apple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PeregruzkaOperatorov
+{
+    //новый класс, наследуемый от product
+    public class Apple : Product
+    {
+        //в классе наследника обязательно должен быть конструктор с такими же параметрами, как у родителя
+        public Apple(string name, int calorie, int volume) : base(name, calorie, volume)
+        {
+            
+        }
+
+        //складываем двав яблока
+        public static Apple Add(Apple apple1, Apple apple2)
+        {
+            int calories = (int)Math.Round(((apple1.Calorie + apple2.Calorie) / 2.0));
+            var volume = apple1.Volume + apple2.Volume;
+            var apple = new Apple("Яблоко", calories, volume);
+            return apple;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PeregruzkaOperatorov
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Apple apple = new Apple("красное яблоко", 100, 100);
+            Apple apple2 = new Apple("зеленое яблоко", 90, 110);
+            
+            
+        }
+    }
+}
